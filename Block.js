45ad2dff22d87a6be924372350bcf3f16dd2ee12
komalswami <komalsswami@gmail.com>
2022-05-16 13:58:35 +0530

    re init

---
+++ Block.js
@@ -0,0 +1,17 @@
+chrome.webRequest.onBeforeRequest.addListener(
+function(details)
+{return{cancle:true}},
+{
+urls:["*://*.zebo.com/*"]
+["*://*.redtube.com/*"]
+["*://*.youjizz.com/*"]
+["*://*.pornhub.com/*"]
+["*://*.meetme.com/*"]
+["*://*.flickr.com/*"]
+["*://*.toomics.com/*"]
+["*://*.thechive.com/*"]
+["*://*.discard & discordapp.com/*"]
+["*://*.snapchart.com/*"]
+["*://*.snapchat.com/*"]
+},[blocking],
+)
